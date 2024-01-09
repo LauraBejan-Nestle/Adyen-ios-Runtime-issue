4b04e6fb86a83105c74eff799e5faa29e6b9752e
LauraBejan-Nestle <155165646+LauraBejan-Nestle@users.noreply.github.com>
2024-01-09 11:26:50 +0200

    Added App with Framework that imports AdyenDropIn

---
+++ Test App/FrameworkA/FrameworkA/AdyenComponent.swift
@@ -0,0 +1,16 @@
+//
+//  AdyenComponent.swift
+//  FrameworkA
+//
+//  Created by Laura Bejan on 09.01.2024.
+//
+
+import Adyen
+import AdyenDropIn
+
+struct AdyenComponent {
+    
+    func doSomething() {
+        _ = DropInComponent.Configuration()
+    }
+}
